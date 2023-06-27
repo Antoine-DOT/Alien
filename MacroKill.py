daba98f3e908b32b9a3f486374667f63977afb79
Antoine <aleflonpro@gmail.com>
2023-06-27 16:40:00 +0200

    amélioration du script macro

--- MacroKill.py
+++ MacroKill.py
@@ -1,6 +1,5 @@
 import time
 import pyautogui
-import keyboard
 
 # Délai avant de commencer l'automatisation
 time.sleep(5)
@@ -11,22 +10,52 @@ time.sleep(5)
 # Délai pour vous permettre de positionner le curseur de la souris sur la fenêtre
 time.sleep(2)
 
-while True:
-    # Saisie de la commande "zone"
-    pyautogui.typewrite("zone")
-    pyautogui.press("enter")
+# Fonction pour exécuter la macro 1
+def executer_macro_1():
+    for _ in range(30):
+        # Saisie de la commande "zone"
+        pyautogui.typewrite("zone")
+        pyautogui.press("enter")
 
-    # Délai entre les commandes
-    time.sleep(1)
+        # Délai entre les commandes
+        time.sleep(1)
 
-    # Saisie de la commande "meteor"
-    pyautogui.typewrite("meteor")
-    pyautogui.press("enter")
+        # Saisie de la commande "meteor"
+        pyautogui.typewrite("meteor")
+        pyautogui.press("enter")
 
-    # Délai entre les commandes
-    time.sleep(1)
+        # Délai entre les commandes
+        time.sleep(1)
 
-    # Vérification de l'appui sur la touche "Échap" pour arrêter le script
-    if keyboard.is_pressed("esc"):
-        print("Erreur : Le script a été interrompu par l'utilisateur.")
-        break
+# Fonction pour exécuter la macro 2
+def executer_macro_2():
+    for _ in range(30):
+        # Saisie de la commande "zone"
+        pyautogui.typewrite("zone")
+        pyautogui.press("enter")
+
+        # Délai entre les commandes
+        time.sleep(1)
+
+        # Saisie de la commande "spatiofarm"
+        pyautogui.typewrite("spatiofarm")
+        pyautogui.press("enter")
+
+        # Délai entre les commandes
+        time.sleep(1)
+
+# Demande à l'utilisateur de choisir une macro
+print("Choisissez une macro à exécuter :")
+print("1. Macro 1 (zone + meteor)")
+print("2. Macro 2 (zone + spatiofarm)")
+
+choix = input("Votre choix : ")
+
+# Exécute la macro en fonction du choix de l'utilisateur
+if choix == "1":
+    executer_macro_1()
+elif choix == "2":
+    executer_macro_2()
+else:
+    print("Choix invalide. Le script se termine.")
+
